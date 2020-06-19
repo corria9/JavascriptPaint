a9252d5f20eaa21afe07bd2245d6b67d2de08fc0
Armando Coria <48301256+corria9@users.noreply.github.com>
2020-06-18 23:24:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+document.querySelectorAll("[data-command").forEach(
+    item => {
+      item.addEeventListener("click", e =>{
+
+        console.log(item.getAttribute("data-command"));
+
+      });
+    }
+  );
+
+document.querySelectorAll("[data-tool]").forEach(
+    item => {
+      item.addEeventListener("click", e =>{
+
+        console.log(item.getAttribute("data-tool"));
+
+      });
+    }
+  );
